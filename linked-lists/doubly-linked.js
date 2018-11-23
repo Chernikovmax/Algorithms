351e8c3545ef921e24fdbc1b8198fd5cfb168d63
Chernikovmax <chernik.max@gmail.com>
2018-11-23 18:39:05 +0400

    Made some remoмштп methods for DoublyLinkedList class.

--- linked-lists/doubly-linked.js
+++ linked-lists/doubly-linked.js
@@ -26,6 +26,9 @@ class DoublyLinkedList {
       this.length++;
     }
   }
+  append(...values) {
+    return values.forEach(value => this._appendItem(value));
+  }
 
   _appToHead(value) {
     let newNode = new Node(value);
@@ -38,20 +41,18 @@ class DoublyLinkedList {
     return values.forEach(value => this._appToHead(value));
   }
 
-  append(...values) {
-    return values.forEach(value => this._appendItem(value));
-  }
   _appendSingleAt(position, value) {
-    let newNode = new Node(value);
-    let current = this.head;
-    let counter = 1;
-
     if (position === this.length-1) {
       return this.append(value);
     }
     if (position === 0) {
       return this._appToHead(value);
     }
+
+    let newNode = new Node(value);
+    let current = this.head;
+    let counter = 1;
+
     if (position <= (this.length-1 / 2)) {
       while(current) {
         current = current.next;
@@ -88,9 +89,90 @@ class DoublyLinkedList {
       i++;
     });
   }
+
+  removeFromHead() {
+    this.head.next.prev = this.head.prev;
+    this.head = this.head.next;
+    this.length--;
+  }
+
+  removeFromTail() {
+    this.tail.prev.next = this.tail.next;
+    this.tail = this.tail.prev;
+    this.length--;
+  }
+
+  _remove(value) {
+    let current = this.head;
+    if (value === this.head.value) {
+      return this.removeFromHead();
+    }
+    if (value === this.tail.value) {
+      return this.removeFromTail();
+    }
+    while (current) {
+      if (value === current.value) {
+        current.prev.next = current.next;
+        current.next.prev = current.prev;
+        this.length--;
+        return this;
+      }
+      current = current.next;
+    }
+  }
+  remove(...values) {
+    return values.forEach(value => this._remove(value));
+  }
+
+  removeAt(position) {
+    if (position === 0) {
+      return removeFromHead();
+    }
+    if (position === this.length - 1) {
+      return removeFromTail();
+    }
+
+    let current = this.head;
+    let counter = 1;
+
+    if (position <= (this.length-1 / 2)) {
+
+      while (current) {
+        current = current.next;
+        if (counter === position) {
+          current.prev.next = current.next;
+          current.next.prev = current.prev;
+          this.length--;
+          return this;
+        }
+        counter++;
+      }
+    }
+
+    if (position >= (this.length-1 / 2)) {
+      current = this.tale;
+      while (current) {
+        current = current.prev;
+        if (counter === position) {
+          current.next.prev = current.prev;
+          current.prev.next = current.next;
+          this.length--;
+          return this;
+        }
+        counter++;
+      }
+    }
+  }
+  // removeAt(position, ...values) {
+  //   let i = position;
+  //   return values.forEach((value) => {
+  //     this.removeAt(i, value);
+  //     i++;
+  //   });
+  // }
 }
 
 const list = new DoublyLinkedList('1', '2', '3', '4');
 
-console.log(list.appendAt(0, 'хуй', 'пизда', 'Джигурда'));
-console.log(list);
+
+console.log(list.removeAt(1, ));
